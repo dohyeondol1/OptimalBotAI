e46d04275a76d4321d1f143dc29007b8a1444c3c
Do Hyeon Seok <dohyeondol@gmail.com>
2024-06-14 22:43:09 +0900

    [FIX] set_page_config 함수 에러 해결
    streamlit의 set_page_config 함수는 반드시 스크립트의 맨 위에서 다른 streamlit 명령어가 호출되기 전에 실행되어야 하는데, 위에 다른 streamlit 명령어가 존재해서 에러가 발생했습니다. 명령어들의 위치를 수정해서 해결하였습니다.

--- 선픽.py
+++ 선픽.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-# 사이드바에 들어가는 타이틀
-st.sidebar.title('OptimalBotAI')
-
 # Streamlit 페이지 구성
 st.set_page_config(layout="wide")
+
+# 사이드바에 들어가는 타이틀
+st.sidebar.title('OptimalBotAI')
 
 # Streamlit 페이지에 CSS 적용하기!
 st.markdown(
