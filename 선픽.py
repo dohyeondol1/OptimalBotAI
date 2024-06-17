ee1a4b6af8a613eea8d0a467364cb55ce2b6f9a7
joeyounghoon <yhjoe36511@gmail.com>
2024-06-17 23:06:05 +0900

    FEATURE/#11/add_champions_list
    add champions_image_list

--- 선픽.py
+++ 선픽.py
@@ -18,6 +18,7 @@ st.markdown(
         display: flex;
         flex-direction: row;
         height: 100vh;
+        justify-content: center; /* 가운데 정렬 추가 */
     }
     .sidebar {
         padding: 10px;
@@ -34,13 +35,14 @@ st.markdown(
         display: flex;
         flex-direction: column;
         margin-bottom: 20px;
+        align-items: center; /* 가운데 정렬 추가 */
     }
     .champion-list-content {
         width: 300px;
         height: 350px;
         display: flex;
         flex-direction: column;
-        item-align: center;
+        align-items: center;
         gap: 10px;
     }
     .recommend-champ-title {
@@ -59,7 +61,7 @@ st.markdown(
         background-color: black;
     }
 
-    /*미디어 쿼리 패딩 제거 */
+    /* 미디어 쿼리 패딩 제거 */
     @media (min-width: 576px) {
         .st-emotion-cache-1jicfl2 {
             padding-left: 0 !important;
@@ -75,12 +77,12 @@ st.markdown(
         max-width: initial;
     }
 
-    /*streamlit 기본 상단 바 제거*/
+    /* streamlit 기본 상단 바 제거 */
     .st-emotion-cache-h4xjwg {
         height: 0;
     }
 
-    /*여백 안남게 scoll-margin 제거*/
+    /* 여백 안남게 scroll-margin 제거 */
     .st-emotion-cache-1jzia57 h1, 
     .st-emotion-cache-1jzia57 h2, 
     .st-emotion-cache-1jzia57 h3, 
@@ -91,21 +93,27 @@ st.markdown(
         scroll-margin-top: 0;
     }
 
-    /*streamlit에서 기본으로 제공하는 공유, 깃허브 링크 등등 바 설정*/
+    /* streamlit에서 기본으로 제공하는 공유, 깃허브 링크 등등 바 설정 */
     .st-emotion-cache-15ecox0.ezrtsby0 {
         background-color: black;
         border-radius: 10px;
     }
 
-    /*화면 상단 gap 제거*/
+    /* 화면 상단 gap 제거 */
     .st-emotion-cache-1wmy9hl div {
         gap: 0;
     }
-    
 
-    /*여백 제거*/
+    /* 여백 제거 */
     .st-emotion-cache-ul70r3 {
         margin-bottom: 0;
+    }
+
+    .champion-image {
+        border-radius: 50%;
+        width: 160px;
+        height: 160px;
+        object-fit: cover;
     }
     </style>
     """, unsafe_allow_html=True
@@ -129,3 +137,69 @@ st.markdown(
     </div>
     """, unsafe_allow_html=True
 )
+
+champions_ad=[{
+    "name" : "애쉬",
+    "image_url":"https://opgg-static.akamaized.net/meta/images/lol/14.12.1/champion/Ashe.png?image=c_crop,h_103,w_103,x_9,y_9/q_auto:good,a_0,f_webp,w_160,h_160&v=1717557723274"
+},
+{
+    "name":"카이사",
+    "image_url":"https://opgg-static.akamaized.net/meta/images/lol/14.12.1/champion/Kaisa.png?image=c_crop,h_103,w_103,x_9,y_9/q_auto:good,a_0,f_webp,w_160,h_160&v=1717557723274"
+},
+{
+    "name":"케이틀린",
+    "image_url":"https://opgg-static.akamaized.net/meta/images/lol/14.12.1/champion/Caitlyn.png?image=c_crop,h_103,w_103,x_9,y_9/q_auto:good,a_0,f_webp,w_160,h_160&v=1717557723274"
+},
+{
+    "name":"징크스",
+    "image_url":"https://opgg-static.akamaized.net/meta/images/lol/14.12.1/champion/Jinx.png?image=c_crop,h_103,w_103,x_9,y_9/q_auto:good,a_0,f_webp,w_160,h_160&v=1717557723274"
+},
+{
+    "name":"진",
+    "image_url":"https://opgg-static.akamaized.net/meta/images/lol/14.12.1/champion/Jhin.png?image=c_crop,h_103,w_103,x_9,y_9/q_auto:good,a_0,f_webp,w_160,h_160&v=1717557723274"
+},
+{
+    "name":"자야",
+    "image_url":"https://opgg-static.akamaized.net/meta/images/lol/14.12.1/champion/Xayah.png?image=c_crop,h_103,w_103,x_9,y_9/q_auto:good,a_0,f_webp,w_160,h_160&v=1717557723274"
+},
+{
+    "name":"바루스",
+    "image_url":"https://opgg-static.akamaized.net/meta/images/lol/14.12.1/champion/Varus.png?image=c_crop,h_103,w_103,x_9,y_9/q_auto:good,a_0,f_webp,w_160,h_160&v=1717557723274"
+},
+{
+    "name":"이즈리얼",
+    "image_url":"https://opgg-static.akamaized.net/meta/images/lol/14.12.1/champion/Ezreal.png?image=c_crop,h_103,w_103,x_9,y_9/q_auto:good,a_0,f_webp,w_160,h_160&v=1717557723274"
+},
+{
+    "name":"사미라",
+    "image_url":"https://opgg-static.akamaized.net/meta/images/lol/14.12.1/champion/Samira.png?image=c_crop,h_103,w_103,x_9,y_9/q_auto:good,a_0,f_webp,w_160,h_160&v=1717557723274"
+},
+{
+    "name":"베인",
+    "image_url":"https://opgg-static.akamaized.net/meta/images/lol/14.12.1/champion/Vayne.png?image=c_crop,h_103,w_103,x_9,y_9/q_auto:good,a_0,f_webp,w_160,h_160&v=1717557723274"
+},
+{
+    "name":"스몰더",
+    "image_url":"https://opgg-static.akamaized.net/meta/images/lol/14.12.1/champion/Smolder.png?image=c_crop,h_103,w_103,x_9,y_9/q_auto:good,a_0,f_webp,w_160,h_160&v=1717557723274"
+},
+{
+    "name":"제리",
+    "image_url":"https://opgg-static.akamaized.net/meta/images/lol/14.12.1/champion/Zeri.png?image=c_crop,h_103,w_103,x_9,y_9/q_auto:good,a_0,f_webp,w_160,h_160&v=1717557723274"
+},
+{
+    "name":"드레이븐",
+    "image_url":"https://opgg-static.akamaized.net/meta/images/lol/14.12.1/champion/Draven.png?image=c_crop,h_103,w_103,x_9,y_9/q_auto:good,a_0,f_webp,w_160,h_160&v=1717557723274"
+},
+{
+    "name":"루시안",
+    "image_url":"https://opgg-static.akamaized.net/meta/images/lol/14.12.1/champion/Lucian.png?image=c_crop,h_103,w_103,x_9,y_9/q_auto:good,a_0,f_webp,w_160,h_160&v=1717557723274"
+},              
+]
+
+# 중앙 정렬을 위한 컨테이너
+with st.container():
+    cols = st.columns(5)
+    for i in range(len(champions_ad)):
+        with cols[i % 5]:
+            champion_ad = champions_ad[i]
+            st.image(champion_ad["image_url"], caption=champion_ad["name"])
